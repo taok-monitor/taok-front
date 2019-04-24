f9485f8f18615be2c00f537df72d8ecb24c78dcb
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2019-04-24 19:41:56 -0300

    removed container tag

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container} from 'reactstrap';
 import Login from './js/pages/Login.jsx'
 
 ReactDOM.render(
-    <div>
-        <Container>
-            <Login/>
-        </Container>        
-    </div>,
+    <Login />,
     document.getElementById('create-article-form')
 )
