c509fdefe95f24cb934a81e1797d9560efb6958c
Mattheus CAssundé <mattheus.089@gmail.com>
2019-07-18 19:40:29 -0300

    feat(Legends): refactoring legends in platform
    - moving legends from the bar component to withBarHOC
    - changing legends returned in withBarHOC

--- src/js/components/Barra/index.js
+++ src/js/components/Barra/index.js
@@ -1,51 +1,10 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Table } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import { Bar } from 'react-chartjs-2';
 
 import Cartao from '../Cartao';
 import { withBarHOC } from '../HOCS/withBarHOC';
-
-const legendas = [
-    {
-        label: 'FME',
-        legenda: 'Fundo Municipal de Educação',
-    },
-    {
-        label: 'HDEAYRE',
-        legenda: 'Frotinha do Ant Bezerra',
-    },
-    {
-        label: 'HDMSJB',
-        legenda: 'Frotinha da Parangaba',
-    },
-    {
-        label: 'HDNSCON',
-        legenda: 'Hosp. Nossa Sra Cj Ceará',
-    },
-    {
-        label: 'HEBO',
-        legenda: 'Frotinha da Messajana',
-    },
-    {
-        label: 'HDGM/ME',
-        legenda: 'Gonzaginha da Messajana',
-    },
-    {
-        label: 'GP',
-        legenda: 'Gabinete do Prefeito',
-    },
-    {
-        label: 'GVP',
-        legenda: 'Gabinete do Vice Prefeito',
-    },
-];
-
-function encontraLegenda(label) {
-    const legendaEncontrada = legendas.find(legenda => legenda.label === label);
-
-    return legendaEncontrada && legendaEncontrada.legenda;
-}
 
 function Barra(props) {
     const { labels, valores, totalAcumulado, getFormattedLabel } = props;
@@ -85,28 +44,6 @@ function Barra(props) {
                         orgao="Total dos 5 Maiores"
                         valor={totalAcumulado}
                     />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Sigla</th>
-                                <th>Legenda</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {labels.map((label, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{label}</td>
-                                        <td>{encontraLegenda(label)}</td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </Table>
                 </Col>
             </Row>
         </Fragment>
--- src/js/components/HOCS/withBarHOC.js
+++ src/js/components/HOCS/withBarHOC.js
@@ -4,6 +4,108 @@ import axios from 'axios';
 
 import * as Util from '../Util';
 
+const legendas = [
+    {
+        label: 'FAS',
+        legenda: 'FUNDO MUNICIPAL DE ASSISTENCIA SOCIAL',
+    },
+    {
+        label: 'FMC',
+        legenda: 'FUNDO MUNICIPAL DE CULTURA',
+    },
+    {
+        label: 'FMDCA',
+        legenda:
+            'FUNDO MUNICIPAL DE DEFESA DOS DIREITOS DA CRIANCA E DO ADOLESCENTE',
+    },
+    {
+        label: 'FMDSE',
+        legenda: 'FUNDO MUNICIPAL DE DESENVOLVIMENTO ECONOMICO',
+    },
+    {
+        label: 'FUNDURB',
+        legenda: 'FUNDO MUNICIPAL DE DESENVOLVIMENTO URBANO',
+    },
+    {
+        label: 'FMDD',
+        legenda: 'FUNDO MUNICIPAL DE DIREITOS DIFUSOS',
+    },
+    {
+        label: 'FME',
+        legenda: 'FUNDO MUNICIPAL DE EDUCACAO',
+    },
+    {
+        label: 'FMHIS',
+        legenda: 'FUNDO MUNICIPAL DE HABITACAO DE INTERESSE SOCIAL',
+    },
+    {
+        label: 'FEMS',
+        legenda: 'FUNDO MUNICIPAL DE SAUDE',
+    },
+    {
+        label: 'FMDPI',
+        legenda: 'FUNDO MUNICIPAL DOS DIREITOS DA PESSOA IDOSA',
+    },
+    {
+        label: 'FMDPD',
+        legenda:
+            'FUNDO MUNICIPAL PARA PROMOCAO DOS DIREITOS DAS PESSOAS COM DEFICIENCIA',
+    },
+    {
+        label: 'GP',
+        legenda: 'GABINETE DO PREFEITO',
+    },
+    {
+        label: 'GVP',
+        legenda: 'GABINETE DO VICE-PREFEITO',
+    },
+    {
+        label: 'GMF',
+        legenda: 'GUARDA MUNICIPAL DE FORTALEZA',
+    },
+    {
+        label: 'HDMSJB',
+        legenda: 'HOSPITAL DISTRITAL MARIA JOSE BARROSO DE OLIVEIRA',
+    },
+    {
+        label: 'HEBO',
+        legenda: 'HOSPITAL DISTRITAL EDMILSON BARROS DE OLIVEIRA',
+    },
+    {
+        label: 'HDEAYRE',
+        legenda: 'HOSPITAL DISTRITAL EVANDRO AYRES DE MOURA',
+    },
+    {
+        label: 'HDGM/BC',
+        legenda: 'HOSPITAL DISTRITAL GONZAGA MOTA/BARRA DO CEARA',
+    },
+    {
+        label: 'HDGM/JW',
+        legenda: 'HOSPITAL DISTRITAL GONZAGA MOTA/JOSE WALTER',
+    },
+    {
+        label: 'HDGM/ME',
+        legenda: 'HOSPITAL DISTRITAL GONZAGA MOTA/MESSEJANA',
+    },
+    {
+        label: 'HDNSCON',
+        legenda: 'HOSPITAL DISTRITAL NOSSA SENHORA DA CONCEICAO',
+    },
+
+    {
+        label: 'HMZAN',
+        legenda: 'HOSPITAL E MATERNIDADE DRA ZILDA ARNS NEUMANN',
+    },
+    {
+        label: 'CROA',
+        legenda: 'HOSPITAL LUCIA DE FATIMA/CROA',
+    },
+    {
+        label: 'IJF',
+        legenda: 'INSTITUTO DR. JOSE FROTA',
+    },
+];
+
 function calculaAcumulado(valores = []) {
     const total = valores.reduce(
         (valorFinal, atual) => valorFinal + atual,
@@ -13,9 +115,17 @@ function calculaAcumulado(valores = []) {
     return Util.formataParaReal(total);
 }
 
-function getFormattedLabel(tooltipItem, data) {
-    const { label, data: dataValues } = data.datasets[tooltipItem.datasetIndex];
+function encontraLegenda(label) {
+    const legendaEncontrada = legendas.find(legenda => legenda.label === label);
 
+    return legendaEncontrada && legendaEncontrada.legenda
+        ? legendaEncontrada.legenda
+        : '';
+}
+
+function getFormattedLabel(tooltipItem, data) {
+    const { data: dataValues } = data.datasets[tooltipItem.datasetIndex];
+    const label = encontraLegenda(data.labels[tooltipItem.index]);
     const value = dataValues[tooltipItem.index];
 
     return `${label}: ${Util.formataParaReal(value)}`;
