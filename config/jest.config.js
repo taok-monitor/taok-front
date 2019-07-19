255fefa196bdd9c0c887f91d5be0ff0045d3a126
Samuel SIlva <samuelsilvawb@gmail.com>
2019-07-19 23:11:01 +0100

    chore: Create files to run tests
    - Installed dependencies, jest, testing library.
    - Created files and commands to execute tests.

---
+++ config/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    clearMocks: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
+    coverageDirectory: 'coverage',
+    moduleFileExtensions: ['js', 'json', 'jsx'],
+    setupFiles: [],
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+    roots: ['<rootDir>', './../src'],
+    testEnvironment: 'jsdom',
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+    testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+    testURL: 'http://localhost',
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    verbose: false,
+};
