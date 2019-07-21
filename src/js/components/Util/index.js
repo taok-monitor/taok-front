3c2dd256d653b1a3c42a37dd443871cd3c7f1d91
Samuel SIlva <samuelsilvawb@gmail.com>
2019-07-21 07:52:01 +0100

    test: Add tests to Util module
    - Added tests to cover the two exported functions.

--- src/js/components/Util/index.js
+++ src/js/components/Util/index.js
@@ -1,4 +1,8 @@
 export function formataParaReal(valor) {
+    if (valor !== 0 && !valor) {
+        throw new TypeError('You should pass a valid argument');
+    }
+
     const numero = Number(valor)
         .toFixed(2)
         .split('.');
@@ -9,6 +13,10 @@ export function formataParaReal(valor) {
 }
 
 export function preparaParametrosDeOrgaos(orgaos = []) {
+    if (!Array.isArray(orgaos)) {
+        throw new TypeError('You should pass an array as argument');
+    }
+
     const paramentos = orgaos
         .map((orgao, index, array) => {
             const isLast = index === array.length - 1;
